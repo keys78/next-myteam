f90cf35c148d6e5311f95e4854bec5426ddfe8c8
Emmanuel O <emmanuelo@phreetech.com>
2022-05-21 17:44:28 +0100

    :- switch

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className="sacramento">
-            <section className="gen-wrapper footer-style">
-                <div className="order-1">
+            <section className="gen-wrapper footer-style py-10">
+                <div className="order-1 flex flex-col justify-between">
                     <Link href={'/'}><a><img src="/images/logo.svg" /></a></Link>
                     <div className="flex items-center justify-between" >
                         <Link href="/"><a className="font-normal">home</a></Link>
@@ -17,22 +17,26 @@ const Footer = () => {
                     California 92714<br />
                     Call Us : 949-833-7432
                 </div>
-                <div className="order-3">
-                    <div class="flex items-center space-x-4">
-                        <a href="#">
-                            <img src="./images/icon-facebook.svg" alt="Facebook" />
-                        </a>
-                        <a href="#">
-                            <img src="./images/icon-pinterest.svg" alt="Pinterest" />
-                        </a>
-                        <a href="#">
-                            <img src="./images/icon-twitter.svg" alt="Twitter" />
-                        </a>
+                <div className="order-3 flex flex-col justify-between">
+                    <div className="flex items-center justify-end">
+                        <div class="flex items-center space-x-4">
+                            <a href="#">
+                                <img src="./images/icon-facebook.svg" alt="Facebook" />
+                            </a>
+                            <a href="#">
+                                <img src="./images/icon-pinterest.svg" alt="Pinterest" />
+                            </a>
+                            <a href="#">
+                                <img src="./images/icon-twitter.svg" alt="Twitter" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="order-4 legal">
+                        Copyright 2020. All Rights Reserved
                     </div>
                 </div>
-                <div className="order-4 legal">
-                    Copyright 2020. All Rights Reserved
-                </div>
+
             </section>
         </footer>
 
