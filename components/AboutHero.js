0ab4a18e2d43a083f69dd7b59980e87a58f8d543
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-05-22 23:49:36 +0100

    update

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -1,13 +1,13 @@
 const AboutHero = () => {
     return ( 
         <>
-        <section className="flex lg:text-left text-center lg:flex-row flex-col items-start justify-between gen-wrapper sm:pt-32 pt-24 lg:pb-40 pb-10">
-            <div>
-                <h1 className="about-heading">About</h1>
+        <section className="flex xl:text-left text-center xl:flex-row flex-col items-start justify-between gen-wrapper sm:pt-32 pt-24 xl:pb-40 pb-10">
+            <div className="xl:w-2/12 w-full">
+                <h1 className="about-heading xl:text-left text-center">About</h1>
             </div>
-            <div className="w-full flex items-center lg:justify-end  justify-center lg:pt-0 pt-6">
+            <div className="w-full flex items-center xl:justify-end  justify-center xl:pt-0 pt-6">
                 <div className="about-description">
-                    <div className="style-append-2 mb-16"></div>
+                    <div className="hide-append-2 style-append-2 mb-16"></div>
                     We help companies build dynamic teams made up of top global talent. 
                     Using our network of passionate professionals we drive innovation and deliver incredible outcomes. 
                     Talented, diverse teams shape the best products and experiences. 
